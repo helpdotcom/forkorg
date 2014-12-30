199fe2ef80a0e9f87cb0af428cc7517e506aaa2f
Buster Collings <busterc@gmail.com>
2014-12-30 10:33:08 -0600

    version bump, assert env FORKORG_GITHUB_TOKEN

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+'use strict';
+var gulp = require('gulp');
+var jshint = require('gulp-jshint');
+var stylish = require('jshint-stylish');
+
+var jsFiles = ['./**/*.js', '!./node_modules/**/*.js'];
+var clearScreen = function() {
+  // break things up with a little space
+  process.stdout.write('\u001b[2J');
+};
+
+gulp.task('lint', function() {
+  clearScreen();
+  return gulp.src(jsFiles)
+    .pipe(jshint())
+    .pipe(jshint.reporter(stylish));
+});
+
+gulp.task('default', ['lint']);
